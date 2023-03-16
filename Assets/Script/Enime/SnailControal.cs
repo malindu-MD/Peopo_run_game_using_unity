c43cac132df3e2f7d708b23ebde18c989a8d4136
malindu-MD <it21173790@my.sliit.lk>
2023-03-16 09:47:43 +0530

    commit
    commit

---
+++ Assets/Script/Enime/SnailControal.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailControal : MonoBehaviour
+{
+    public float EnimeSpeed = 5.0f;
+    public Transform bottomcollision;
+    public LayerMask groundlayer;
+    private Rigidbody2D _rBody;
+    public bool _Moveleft;
+
+     private void Start()
+    {
+        _Moveleft = true;
+    }
+
+
+    private void Awake()
+    {
+        _rBody = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        checkcollision();
+        EnimeWalk();
+    }
+
+    private void EnimeWalk()
+    {
+        if (_Moveleft) {
+
+            _rBody.velocity = new Vector2(-EnimeSpeed, _rBody.velocity.y);
+
+            changedirection(3);
+
+        }
+
+        else
+        {
+            _rBody.velocity = new Vector2(EnimeSpeed, _rBody.velocity.y);
+            changedirection(-3);
+        }
+        
+    }
+
+    private void changedirection(int direction)
+    {
+
+        Vector3 tempscale = transform.localScale;
+        tempscale.x = direction;
+        transform.localScale = tempscale;
+
+
+    }
+
+    private void checkcollision()
+    {
+        if (!Physics2D.Raycast(bottomcollision.position, Vector2.down, 0.5f, groundlayer))
+        {
+            Changedirection();
+        }
+        
+
+    }
+
+
+    private void Changedirection()
+    {
+
+        _Moveleft = !_Moveleft;
+
+    }
+
+
+
+
+}
---
+++ Assets/Script/Player/PlayerControal.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControal : MonoBehaviour
+{
+    #region variables
+    public float JumpPower = 7f;
+    public float PlayerSpeed = 30.0f;
+    public Transform groundcheckposition;
+    private Rigidbody2D _rbody;
+
+    public float newjumppowerx;
+
+    public bool _isGrounded;
+
+    private bool _jumped;
+
+    public LayerMask enemy;
+
+    private Animator _anim;
+    #endregion
+
+    
+    private void Awake()
+    {
+
+             _rbody = GetComponent<Rigidbody2D>();
+              _anim = GetComponent<Animator>();
+
+    }//awake
+
+    private void FixedUpdate()
+    {
+        playerwalk();
+    }//fixedupdate
+
+    private void playerwalk()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput>0)//goright
+        {
+            _rbody.velocity = new Vector2(PlayerSpeed, _rbody.velocity.y);
+            _anim.SetInteger("Speed", 1);
+            ChangeDirection(15);
+        }
+        else if (horizontalInput < 0)//goleft
+        {
+            _rbody.velocity = new Vector2(-PlayerSpeed, _rbody.velocity.y);
+            _anim.SetInteger("Speed", 1);
+            ChangeDirection(-15);
+
+        }
+        else
+        {
+            _rbody.velocity = new Vector2(0, _rbody.velocity.y);
+            _anim.SetInteger("Speed", 0);
+
+        }
+
+    }//playerwalk
+
+    private void ChangeDirection(int direction)
+    {
+        Vector3 tempscale = transform.localScale;
+        tempscale.x = direction;
+        transform.localScale = tempscale;
+
+
+
+
+    }//changedirection
+
+    // private void OnCollisionExit2D(Collision2D target)
+    //{
+    //if(target.gameObject.tag== "Enemy")
+    // {
+    //    Destroy(target.gameObject);
+    // }
+    // } 
+
+    //private void OnTriggerEnter2D(Collider2D target)
+    //
+    //if (target.tag == "Enemy")
+    //{
+    // print("hello world");
+    // }
+    // }
+
+
+
+
+
+    private void Update()
+    {
+       
+        PlayerJump();
+
+
+    }
+
+    private void PlayerJump()
+    {
+        if (_isGrounded)
+        {
+              if (Input.GetKey(KeyCode.Space))
+            {
+                _jumped = true;
+                newjumppowerx = (_rbody.velocity.x) + JumpPower;
+
+                _rbody.velocity = new Vector2(newjumppowerx, JumpPower);
+                _anim.SetBool("jump", true);   
+            }
+
+        }
+
+    }
+
+   
+
+
+}//class
